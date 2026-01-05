407fde7f138e46c59efc4adb18de49eac854a2ea
Natars <pasero.romain@gmail.com>
2026-01-05 21:07:01 +0900

    Centralisation du nom des catégories dans le fichier "constants.js" afin de retrouver plus facilement toutes les catégories existantes

--- blocks/constants.js
+++ blocks/constants.js
@@ -2,3 +2,10 @@ export const CATEGORY_COLOUR = {
     ROUTE: 120,
     POLICY_LOGICAL_OPERATOR: 345
 };
+
+export const BLOCK_CATEGORY = {
+    ROUTE_CONTENT: "routeContent",
+    ROUTE_POLICY: "route_policy",
+    ROUTE_OPTIONS: "routeOptions",
+    POLICY_CONDITION: "policy_condition"
+};
