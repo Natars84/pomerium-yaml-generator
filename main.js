e1b0f8b5b70e1fe5f401db77216fc814c097cab0
Natars <pasero.romain@gmail.com>
2026-01-05 22:19:21 +0900

    Remplacement de la toolbox "flyoutToolbox" par la "categoryToolbox" pour facilier la recherche des blocs par catégorie.
    Déplacement de l'import des cutom_blocks directement dans le main.js afin de simplifier les imports de script dans le fichier index.html

--- main.js
+++ main.js
@@ -1,4 +1,7 @@
-const workspace = Blockly.inject('blocklyDiv', {
+import { CATEGORY_COLOUR, BLOCK_CATEGORY } from "./blocks/constants.js";
+import "./blocks/custom_blocks.js";
+
+/* const workspace = Blockly.inject('blocklyDiv', {
   toolbox: {
     "kind": "flyoutToolbox",
     "contents": [
@@ -9,8 +12,34 @@ const workspace = Blockly.inject('blocklyDiv', {
       { "kind": "block", "type": "policy_not" }
     ]
   }
-});
+}); */
 
+const workspace = Blockly.inject('blocklyDiv', {
+  toolbox: {
+    "kind": "categoryToolbox", // Indique qu'on veut un menu avec catégories
+    "contents": [
+      {
+        "kind": "category",
+        "name": "Routes",
+        "colour": CATEGORY_COLOUR.ROUTE, // Vert
+        "contents": [
+          { "kind": "block", "type": "route_rule" }
+        ]
+      },
+      {
+        "kind": "category",
+        "name": "Conditions",
+        "colour": CATEGORY_COLOUR.POLICY_CONDITION, // Bleu
+        "contents": [
+          { "kind": "block", "type": "route_action" },
+          { "kind": "block", "type": "policy_and" },
+          { "kind": "block", "type": "policy_or" },
+          { "kind": "block", "type": "policy_not" }
+        ]
+      }
+    ]
+  }
+});
 /* function generateYaml() {
   // On utilise notre générateur YAML personnalisé
   const code = yamlGenerator.workspaceToCode(workspace);
