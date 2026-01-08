586622dcb50f0868da165246ea3ca049815f18e7
Natars <pasero.romain@gmail.com>
2026-01-08 19:46:24 +0700

    Création d'un script dédié pour contenir les matchers (string, dates, devices, ...)
    Ajout à l'intérieur du matcher de string

---
+++ blocks/matchers.js
@@ -0,0 +1,8 @@
+export const STRING_MATCHERS = [
+    ["est", "is"],
+    ["contient", "contains"],
+    ["commence par", "prefix"],
+    ["se termine par", "suffix"],
+    ["est dans la liste", "is_in"],
+    ["n'est pas dans la liste", "not_in"]
+];
