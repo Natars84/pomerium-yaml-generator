a735132408e86bc65df5aeb8984a4cb95011f236
Natars <pasero.romain@gmail.com>
2026-01-08 18:37:00 +0700

    Mise à jour des couleurs et création d'une catégorie pour les éléments généraux de la configuration

--- blocks/constants.js
+++ blocks/constants.js
@@ -1,9 +1,11 @@
 export const CATEGORY_COLOUR = {
     ROUTE: 120,
-    POLICY_CONDITION: 345
+    POLICY_CONDITION: 270,
+    POLICY_LOGIC: 300
 };
 
 export const BLOCK_CATEGORY = {
+    CONFIG_GENERALE: "generalConfig",
     ROUTE_CONTENT: "routeContent",
     ROUTE_POLICY: "route_policy",
     ROUTE_OPTIONS: "routeOptions",
